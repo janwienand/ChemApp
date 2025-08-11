4879a08383f70fdbe59058ac79797471a4d624a8
Jan <janwienand@gmail.com>
2025-08-11 17:37:10 +0200

    init

--- src/main/java/org/example/ChemCompoundDB.java
+++ src/main/java/org/example/ChemCompoundDB.java
@@ -15,7 +15,7 @@ public class ChemCompoundDB {
     private static final String DB_URL = "jdbc:h2:mem:chemdb;DB_CLOSE_DELAY=-1";
     private static final String DB_USER = "sa";
     private static final String DB_PASSWORD = "very_secret_and_complex_password";
-    private static final String API_SECRET_KEY = "key123_a_very_bad_secret_dont_use";
+    private static final String API_SECRET_KEY = "key123_a_very_bad_secret_dont_use_it";
     private static final String SALT_FOR_HASHING = "static_salt_for_every_user";
 
 
