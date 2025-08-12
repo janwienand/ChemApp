a61c6974b5882637453c9edd40ae9148678a1edd
Jan <janwienand@gmail.com>
2025-08-12 09:57:00 +0200

    add vuln

--- src/main/java/org/example/ChemCompoundDB.java
+++ src/main/java/org/example/ChemCompoundDB.java
@@ -117,6 +117,6 @@ public class ChemCompoundDB {
     }
 
     private void legacyEncrypt() throws NoSuchAlgorithmException, NoSuchPaddingException {
-        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+        Cipher cipher = Cipher.getInstance("DES");
     }
 }
